69b04dba4f4ad0e5d656235685a3cc80f0c565c6
root <root@interop22-idp.itec.tamu.edu>
2022-04-05 13:21:42 -0500

    Working on Interop Demo IDP Deployer

--- opt/shibboleth-idp/edit-webapp/webauthn/js/custom_webauthn.js
+++ opt/shibboleth-idp/edit-webapp/webauthn/js/custom_webauthn.js
@@ -1,3 +1,30 @@
+/**
+ *    * Convince cookie functions
+ **/
+function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+               c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+}
+
+
+
 function rejectIfNotSuccess(response) {
   if (response.success) {
     return response;
